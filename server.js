5736a3c9b1b8cec38ddaac290edc877788be4cb8
Abel Thomas <abelthomas946@gmail.com>
2025-11-25 23:22:53 +0530

    added cors

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://abelthomas-portfolio.vercel.app",
+    methods: ["GET", "POST"],
+  })
+);
 app.use(express.json())
 
 
